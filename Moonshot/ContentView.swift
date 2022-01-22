68333c021466068363cb9c95b3e08308d4353c84
Yury Prokhorov <yury.prokhorov@appewa.com>
2022-01-22 19:07:37 +0300

    Decoding json added

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
 
     
     var body: some View {
